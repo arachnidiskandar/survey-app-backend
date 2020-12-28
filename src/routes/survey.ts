3b6f222e8743ad9cf87d4044d5100f9fed276da2
Augusto <augstarachrafael@gmail.com>
2020-12-28 19:15:42 -0300

    Joi Labels

--- src/routes/survey.ts
+++ src/routes/survey.ts
@@ -7,14 +7,15 @@ import validateBodyRequest from '@middlewares/bodyValidation';
 
 const surveyRoutes = Router();
 
-surveyRoutes.get('', controller.getAllSurveys);
 surveyRoutes.post(
   '',
   verifyAuthorization,
   (req, res, next) => {
     const questionSchema = Joi.object({
-      title: Joi.string().required(),
-      questionType: Joi.string().valid('MULTIPLE CHOICE', 'MULTIPLE SELECT', 'OPEN TEXT', 'YES OR NO'),
+      title: Joi.string().required().label('Question Title'),
+      questionType: Joi.string()
+        .valid('MULTIPLE CHOICE', 'MULTIPLE SELECT', 'OPEN TEXT', 'YES OR NO')
+        .label('Question Type'),
       choices: Joi.when('questionType', {
         is: Joi.string().valid('OPEN TEXT', 'YES OR NO'),
         then: Joi.forbidden(),
@@ -22,10 +23,10 @@ surveyRoutes.post(
       }),
     });
     const surveySchema = Joi.object({
-      title: Joi.string().required(),
-      coordinator: Joi.required(),
-      questions: Joi.array().required().items(questionSchema).min(1),
-      availableToAnyone: Joi.boolean().required(),
+      title: Joi.string().required().label('Survey Title'),
+      coordinator: Joi.required().label('Coordinator'),
+      questions: Joi.array().required().items(questionSchema).min(1).label('Questions'),
+      availableToAnyone: Joi.boolean().required().label('Available to Anyone'),
     });
     validateBodyRequest(surveySchema, req, res, next);
   },
